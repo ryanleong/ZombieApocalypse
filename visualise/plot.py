c9b107f00e22af31eea66d934e5af86c96f6013e
Adam Juraszek <juriad@gmail.com>
2014-09-28 21:05:02 +1000

    Better graphs.
    Humans are drawn on top.
    Infected are scaled.
    Only 10000 values in the plot.

--- visualise/plot.py
+++ visualise/plot.py
@@ -2,6 +2,8 @@
 
 import fileinput
 import sys
+from pprint import pprint
+import math
 
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
@@ -19,13 +21,39 @@ for line in fileinput.input('-'):
 	infectedList.append(int(infected))
 	zombiesList.append(int(zombies))
 
+
+timeList2 = []
+humansList2 = []
+infectedList2 = []
+zombiesList2 = []
+
+length = len(timeList);
+each = math.ceil(length / min(length, 10000))
+for i in [i for i in range(length) if i % each == 0]:
+	timeList2.append(timeList[i])
+	humansList2.append(humansList[i])
+	infectedList2.append(infectedList[i])
+	zombiesList2.append(zombiesList[i])
+
+
+
+
 host = host_subplot(111, axes_class=AA.Axes)
 plt.subplots_adjust(right=0.75)
 
 par1 = host.twinx()
 par2 = host.twinx()
 
-offset = 60
+
+
+entities=[ {'name': "Humans", 'list': humansList2, 'style': "g-", 'zorder': 10, 'coef': 1},
+	{'name': "Zombies", 'list': zombiesList2, 'style': "r-", 'zorder': 5, 'coef': 1},
+	{'name': "Infected", 'list': infectedList2, 'style': "b-", 'zorder': 1, 'coef': 1.4} ]
+plots = [host, par1, par2]
+
+
+
+offset = 80
 new_fixed_axis = par2.get_grid_helper().new_fixed_axis
 par2.axis["right"] = new_fixed_axis(loc="right",
                                     axes=par2,
@@ -33,31 +61,20 @@ par2.axis["right"] = new_fixed_axis(loc="right",
 par2.axis["right"].toggle(all=True)
 
 host.set_xlabel("Time")
-host.set_ylabel("Humans")
-par1.set_ylabel("Infected")
-par2.set_ylabel("Zombies")
+for i in range(len(entities)):
+	plots[i].set_ylabel(entities[i]['name'])
+	p, = plots[i].plot(timeList2, entities[i]['list'], entities[i]['style'], label=entities[i]['name'], zorder=entities[i]['zorder'])
+	entities[i]['plot'] = p
 
-p1, = host.plot(timeList, humansList, 'g-', label='Humans')
-p2, = par1.plot(timeList, infectedList, 'b-', label='Infected')
-p3, = par2.plot(timeList, zombiesList, 'r-', label='Zombies')
-
-mHumans = max(humansList);
-mInfected = max(infectedList);
-mZombies = max(zombiesList);
-
-host.set_ylim(-mHumans*0.2, mHumans*1.2)
-par1.set_ylim(-mInfected*0.2, mInfected*1.2)
-par2.set_ylim(-mZombies*0.2, mZombies*1.2)
-
+	m = max(entities[i]['list']) * entities[i]['coef']
+	plots[i].set_ylim(-0.2*m, 1.2*m)
+	plots[i].axis['left'].label.set_color(p.get_color())
+	plots[i].axis['right'].label.set_color(p.get_color())
+	
 host.legend()
-
-host.axis["left"].label.set_color(p1.get_color())
-par1.axis["right"].label.set_color(p2.get_color())
-par2.axis["right"].label.set_color(p3.get_color())
-
 plt.draw()
 
 if(len(sys.argv) > 1):
-	plt.savefig(sys.argv[1]);
+	plt.savefig(sys.argv[1])
 else:
 	plt.show()
